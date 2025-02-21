f4df972a4ea3e6433d06c853f9fcb423f02e21ba
praveen2git <36920351+praveen2git@users.noreply.github.com>
2025-02-21 08:56:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,1 @@
+![authsecret gen app](https://github.com/akinleyeisrael/openssl/assets/41457034/5dcc42c5-25d0-4c8e-a58c-1681164f1b83)
---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { CopyIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+import { useState } from "react";
+import { toast } from "sonner";
+import Footer from "./Footer";
+
+export default function Home() {
+  const [key, setKey] = useState("")
+
+  const generateOpenssl = () => {
+    const randomBytes = new Uint8Array(44);
+    window.crypto.getRandomValues(randomBytes);
+    const byteArray = Array.from(randomBytes);
+    const base64String = btoa(String.fromCharCode(...byteArray));
+    setKey(base64String)
+  }
+
+  const copyKey = async () => {
+    await navigator.clipboard.writeText(key);
+    toast.info("Copied to clipboard!");
+  }
+  return (
+    <div>
+      <div className="flex flex-col items-center mx-auto space-y-6 mt-32 px-10 sm:mt-52">
+        <h1 className="text-5xl leading-10 tracking-tighter mb-4 font-bold  whitespace-break-spaces">
+          Auth secret generator for next js apps
+        </h1>
+        <Button onClick={generateOpenssl} variant={"secondary"}>Generate</Button>
+        <div className="flex space-x-2 text-primary-foreground relative">
+          <div className="bg-primary border rounded-lg px-6 h-20 w-[20rem] md:w-[40rem] justify-center">
+            <p placeholder="Auth gen" className="max-w-[15rem] my-6 break-before-all break-words text-xs sm:max-w-lg sm:my-8 md:max-w-[30rem]">{key}</p>
+          </div>
+          <CopyIcon onClick={copyKey} className="absolute end-6 m-8 hover:cursor-pointer" />
+        </div>
+      </div>
+      <div className="mt-52 text-center">
+        <Footer />
+      </div>
+    </div>
+  );
+}
